aa8937a20996599abeb57fa9dc6b07583401e30b
Zac Bergquist <zbergquist99@gmail.com>
2016-11-03 18:31:04 -0400

    Ensure that multiple transforms don't share the same name

--- transformation.go
+++ transformation.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/enaml-ops/enaml"
+import (
+	"fmt"
+
+	"github.com/enaml-ops/enaml"
+)
 
 // Transformation is an action applied to a manifest.
 type Transformation interface {
@@ -17,6 +21,9 @@ var transformationBuilders map[string]TransformationBuilder
 func RegisterTransformationBuilder(name string, tb TransformationBuilder) {
 	if transformationBuilders == nil {
 		transformationBuilders = make(map[string]TransformationBuilder)
+	}
+	if _, ok := transformationBuilders[name]; ok {
+		panic(fmt.Errorf("duplicate transformation %q\n\nThis is a development error and should be reported at https://github.com/enaml-ops/omg-transform/issues", name))
 	}
 	transformationBuilders[name] = tb
 }
